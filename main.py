7ccde9dac5d83f152f923f4d23f17d7e32c10ecd
Ikwan Thareq Kemal <ikwanthareq@gmail.com>
2021-09-06 11:24:26 +0000

    create class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+class employee :
+  name = "ikwan"
+  target = 6
+  def checkTarget(self) :
+    if self.target >= 5 :
+      print("Target terpenuhi")
+    else:
+      print("belum mencapai target")
+
+objEmployee = employee()
+print(objEmployee.name)
+objEmployee.checkTarget()
